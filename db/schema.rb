e418e80d8255c046fe1c8946349e225324defb7b
Adam Friedl <ajfriedl@gmail.com>
2017-02-07 22:12:51 -0500

    Basic user creation is working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208020313) do
+ActiveRecord::Schema.define(version: 20170208025700) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
@@ -35,8 +35,7 @@ ActiveRecord::Schema.define(version: 20170208020313) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
+    t.string   "name"
     t.string   "username"
     t.string   "email"
     t.string   "password_digest"
